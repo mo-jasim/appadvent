2bf7abc4102936d6f75472cf95419201a94d2dd9
profertility <profertility@users.noreply.github.com>
2026-04-13 14:31:05 +0530

    New Docker ignore file added

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector, useStore } from 'react-redux'
-import type { AppDispatch, AppStore, RootState } from './store'
+import type { AppDispatch, AppStore, RootState } from './store.ts'
 
 /**
  * Typed Redux Hooks
